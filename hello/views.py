2cd3ed69443ea4ce6670fbacd8370f649fa8a6cc
Дикий Гонзалес <a.vakhno@ukma.edu.ua>
2025-09-09 19:51:18 +0300

    Update text

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1 style="color:green;">Kharkiv TOP:)</h1>')
+    return HttpResponse('<h1 style="color:red;">Kharkiv TOP:)</h1>')
 
 
 def fancy_index(request):
